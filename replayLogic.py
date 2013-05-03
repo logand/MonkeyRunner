f8074e03dbe488e5b42de2aaf11ae741558b2407
LoganD <logandonovan@gmail.com>
2013-05-03 03:35:40 -0400

    adding more error checking

--- replayLogic.py
+++ replayLogic.py
@@ -1,17 +1,23 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
-import sys
+import sys, os, time
 from com.xhaus.jyson import JysonCodec as json
 
 def run_input(action, newdevice, test):
     actionComplete = True
     if action['type'] == 'touch':
-        counter = (action['up'] - action['down'])/1000
-        if test:
-            print 'touch at (' + str(action['x']) + ", " + str(action['y']) + ") for " + str(counter) + " seconds"
+        if 'x' in action and 'y' in action and 'up' in action and 'down' in action:
+            counter = (action['up'] - action['down'])/1000
+            if test:
+                print 'touch at (' + str(action['x']) + ", " + str(action['y']) + ") for " + str(counter) + " seconds"
+            else:
+                if str(action['x']).isdigit() and str(action['y']).isdigit():
+                    newdevice.touch(action['x'], action['y'], 'DOWN')
+                    MonkeyRunner.sleep(counter)
+                    newdevice.touch(action['x'], action['y'], 'UP')
+                else:
+                    actionComplete = False
         else:
-            newdevice.touch(action['x'], action['y'], 'DOWN')
-            MonkeyRunner.sleep(counter)
-            newdevice.touch(action['x'], action['y'], 'UP')
+            actionComplete = False
         
     elif action['type'] == 'drag':
         counter = action['up'] - action['down']
@@ -41,27 +47,52 @@ def run_input(action, newdevice, test):
 
 def run_jblock(filename, newdevice):
     f = open(filename, 'r')
+    '''
+    except IOError: 
+        print 'problem reading:' + filename
+    '''
     print "opened file"
     totalCompleted = 0
     totalActions = 0
+    '''
+    if not os.path.exists('./images'):
+        os.mkdir('./images')
     newdevice.wake()
+    pathName = os.path.abspath('./images')
+    #print pathName
+    startShot = os.path.join(pathName, 'start.png')
+    screenshot = newdevice.takeSnapshot()
+    screenshot.writeToFile(startShot)
+    '''
     for line in f:
         totalActions += 1
-        device_input = json.loads(line)
-        complete = run_input(device_input, newdevice, False)
-        if complete:
-            totalCompleted += 1
+        singleQuotes = line.find("\'")
+        if singleQuotes == -1:
+            device_input = json.loads(line)
+            complete = run_input(device_input, newdevice, False) #change to True to 
+            if complete:
+                totalCompleted += 1
+            else:
+                action = str(device_input).replace(': u', ': ')
+                print 'could not replay action ' + str(action)
         else:
-            action = str(device_input).replace(': u', ': ')
-            print 'could not replay action ' + str(action)
+            print 'could not replay action ' + line
     print str(totalCompleted)+ '/' + str(totalActions) + ' actions completed'
+    '''
+    time.sleep(5)
+    screenshotFinal = newdevice.takeSnapshot()
+    finishShot = os.path.join(pathName, 'finish.png')
+    screenshotFinal.writeToFile(finishShot)
+    same = screenshot.sameAs(screenshotFinal, 0.999)
+    print 'The images are the same: ' + str(same)
+    '''
 
 def main():
     if len(sys.argv) == 1:
         filename = 'testLogicLog.txt'
     else:
         filename = sys.argv[1]
-    newdevice = MonkeyRunner.waitForConnection()  
+    newdevice = MonkeyRunner.waitForConnection(5.0)  
     run_jblock(filename, newdevice)
     print 'done'
 
